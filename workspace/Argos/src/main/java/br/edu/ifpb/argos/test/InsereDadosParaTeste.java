36ec4306ea41072f8a9411cd3a2f89daf9784f8a
Hilberto Fernandes <hilbertofernandes@gmail.com>
2018-11-04 15:38:09 -0300

    Argos

--- workspace/Argos/src/main/java/br/edu/ifpb/argos/test/InsereDadosParaTeste.java
+++ workspace/Argos/src/main/java/br/edu/ifpb/argos/test/InsereDadosParaTeste.java
@@ -42,20 +42,35 @@ public class InsereDadosParaTeste {
 	public void testInsereUsuarios() throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
+		
+		//inserindo um usuário Administrador
 		Usuario usuario1 = new Usuario();
 		usuario1.setMatricula("123456");
-		usuario1.setNome("argos");
+		usuario1.setNome("Argos - Administrador");
 		usuario1.setEmail("argos@ifpb.edu.br");
 		usuario1.setSenha("123456");
 		usuario1.setFone("83996193926");
 		Date aniversario1 = sdf.parse("1981-06-15");
 		usuario1.setDataAniversario(aniversario1);
 		usuario1.setStatus(true);
-		usuario1.setPerfil("funcionario");
+		usuario1.setPerfil("administrador");
+		
+		//inserindo um usuário comum
+		Usuario usuario2 = new Usuario();
+		usuario2.setMatricula("246810");
+		usuario2.setNome("Argos - Comum");
+		usuario2.setEmail("argos@ifpb.edu.br");
+		usuario2.setSenha("123456");
+		usuario2.setFone("83996193926");
+		Date aniversario2 = sdf.parse("1981-06-15");
+		usuario2.setDataAniversario(aniversario2);
+		usuario2.setStatus(true);
+		usuario2.setPerfil("usuario");
 
 		UsuarioDAO udao = new UsuarioDAO(em);
 		udao.beginTransaction();
 		udao.insert(usuario1);
+		udao.insert(usuario2);
 		udao.commit();
 
 	}
